06edba95f360f33c9b21196f1e7c63e97333f9cc
Matthew Chen <matthew@signal.org>
2018-09-25 14:48:53 -0400

    Remove Xcode project and podfile.

--- Pods/Target Support Files/Pods-SignalCoreKit/Pods-SignalCoreKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SignalCoreKit : NSObject
-@end
-@implementation PodsDummy_Pods_SignalCoreKit
-@end
--- Pods/Target Support Files/Pods-SignalCoreKitTests/Pods-SignalCoreKitTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SignalCoreKitTests : NSObject
-@end
-@implementation PodsDummy_Pods_SignalCoreKitTests
-@end
