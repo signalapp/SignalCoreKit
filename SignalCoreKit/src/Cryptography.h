b9d95003c9d879f16ca783590aa5bfe813258a7e
Michael Kirk <michael.code@endoftheworl.de>
2019-05-13 10:34:05 -0600

    Specify shouldPad param

--- SignalCoreKit/src/Cryptography.h
+++ SignalCoreKit/src/Cryptography.h
@@ -107,6 +107,7 @@ typedef NS_ENUM(NSInteger, TSMACType) {
                                   error:(NSError **)error;
 
 + (nullable NSData *)encryptAttachmentData:(NSData *)attachmentData
+                                 shouldPad:(BOOL)shouldPad
                                     outKey:(NSData *_Nonnull *_Nullable)outKey
                                  outDigest:(NSData *_Nonnull *_Nullable)outDigest;
 
