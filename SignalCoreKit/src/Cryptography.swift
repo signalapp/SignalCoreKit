d2a6e248fb560b2b76c8ef15ed203e2377f9bb91
Nora Trapp <nora@signal.org>
2019-06-19 13:11:53 -0700

    PR Feedback

--- SignalCoreKit/src/Cryptography.swift
+++ SignalCoreKit/src/Cryptography.swift
@@ -7,6 +7,26 @@ import CommonCrypto
 
 public extension Cryptography {
     class func pbkdf2Derivation(password: Data, salt: Data, iterations: UInt32, outputLength: Int) -> Data? {
+        guard password.count > 0 else {
+            owsFailDebug("Invalid password.")
+            return nil
+        }
+
+        guard salt.count > 0 else {
+            owsFailDebug("Invalid salt.")
+            return nil
+        }
+
+        guard iterations > 0 else {
+            owsFailDebug("Invalid iterations.")
+            return nil
+        }
+
+        guard outputLength > 0 else {
+            owsFailDebug("Invalid outputLength.")
+            return nil
+        }
+
         let passwordBytes: [Int8] = password.withUnsafeBytes {
             [Int8]($0.bindMemory(to: Int8.self))
         }
