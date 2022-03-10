2a1b768441716772cbe3c66cbce9521de87f533e
Martin Böttcher <martin.boettcher@greenbytes.de>
2022-03-10 11:04:17 +0100

    added support for using localized strings from the bundle of the main app (when running in an app extension)

--- SignalCoreKit/src/SignalCoreKit.h
+++ SignalCoreKit/src/SignalCoreKit.h
@@ -13,4 +13,9 @@ FOUNDATION_EXPORT const unsigned char SignalCoreKitVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SignalCoreKit/PublicHeader.h>
 #import <SignalCoreKit/Cryptography.h>
 #import <SignalCoreKit/SCKError.h>
-#import <signalCoreKit/UnfairLock.h>
+#import <SignalCoreKit/UnfairLock.h>
+
+#ifdef __OBJC__
+    #define OWSLocalizedString(key, comment) \
+        [[NSBundle mainBundle].app localizedStringForKey:(key) value:@"" table:nil]
+#endif
